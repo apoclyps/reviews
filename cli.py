d67ca8c1bfb0bf809aa2feb610077e1ab54d588f
apoclyps <kyle90adam@hotmail.com>
2021-05-08 15:27:04 +0100

    improve contextual help in CLI

--- cli.py
+++ cli.py
@@ -13,13 +13,23 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(__version__, "-v", "--version", message="version %(version)s")
 async def main():
-    """Click entrypoint with help provided by default."""
+    """Reviews - A terminal UI Dashboard for monitoring code review requests.\n
+
+    For feature requests or bug reports: https://github.com/apoclyps/reviews/issues
+    """
 
 
-@main.command(help="Display a dashboard")
+@main.command(help="Visualize code review requests as a Dashboard")
 @click.option("-r", "--reload/--no-reload", default=True, is_flag=True)
 async def dashboard(reload):
-    """Dashboard command."""
+    """
+    Command:\n
+        reviews dashboard
+
+    Usage:\n
+        reviews dashboard --reload \n
+        reviews dashboard --no-reload \n
+    """
 
     click.echo("loading dashboard")
 
