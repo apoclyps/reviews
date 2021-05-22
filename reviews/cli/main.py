ec0cec06d0db0da7073e9ca2033a1ebd31fe2173
Kyle Harrison <kyle90adam@hotmail.com>
2021-05-22 12:29:06 +0100

    replaces asyncclick with click (#131)

--- reviews/cli/main.py
+++ reviews/cli/main.py
@@ -1,6 +1,4 @@
-import asyncio
-
-import asyncclick as click
+import click
 
 from ..tasks import render, single_render
 from ..version import __version__
@@ -10,7 +8,7 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(__version__, "-v", "--version", message="version %(version)s")
-async def cli() -> None:
+def cli() -> None:
     """Reviews - A terminal UI Dashboard for monitoring code review requests.\n
 
     For feature requests or bug reports: https://github.com/apoclyps/reviews/issues
@@ -19,7 +17,7 @@ async def cli() -> None:
 
 @cli.command(help="Visualize code review requests as a Dashboard")
 @click.option("-r", "--reload/--no-reload", default=True, is_flag=True)
-async def dashboard(reload: bool) -> None:
+def dashboard(reload: bool) -> None:
     """
     Command:\n
         reviews dashboard
@@ -32,14 +30,11 @@ async def dashboard(reload: bool) -> None:
     click.echo("loading dashboard")
 
     if reload:
-        await asyncio.gather(
-            # asyncio.to_thread(update),
-            asyncio.to_thread(render),
-        )
+        render()
     else:
         single_render()
 
 
 def main() -> None:
     """Entry point to CLI"""
-    asyncio.run(cli())
+    cli()
