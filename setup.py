0ad343f8674d036d00e45feca0bbaa840fbb2b5d
Kyle Harrison <kyle90adam@hotmail.com>
2021-05-31 12:51:56 +0100

    Bump version: 0.1.9 → 0.1.10 (#147)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 setup(
     name="reviews",
     packages=find_namespace_packages(include=["*"]),
-    version="0.1.9",
+    version="0.1.10",
     license="MIT",
     description=(
         "A terminal UI dashboard to monitor requests for code "
