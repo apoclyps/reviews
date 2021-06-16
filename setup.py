dbacebab923d1f7ec3f40d985d1ecf3bb53cd2d1
apoclyps <kyle90adam@hotmail.com>
2021-06-16 21:53:00 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 setup(
     name="reviews",
     packages=find_namespace_packages(include=["*"]),
-    version="0.2.0",
+    version="0.2.1",
     license="MIT",
     description=("A terminal UI dashboard to monitor requests for code " "review across Github repositories."),
     author="Kyle Harrison",
