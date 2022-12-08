773baae61cd9f7a3065788d313d1544af36d672b
Kyle Harrison <kyle90adam@hotmail.com>
2022-12-08 09:06:19 +0000

    Bump version: 0.5.0 → 0.5.1 (#585)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md") as fh:
 setup(
     name="reviews",
     packages=find_namespace_packages(include=["*"]),
-    version="0.5.0",
+    version="0.5.1",
     license="MIT",
     description=("A terminal UI dashboard to monitor requests for code " "review across Github repositories."),
     author="Kyle Harrison",
